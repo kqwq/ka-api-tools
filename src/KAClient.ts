c0c2442dbaf39aedabf8a2b372f69ee22d5d1b08
irateanimal <irateanimal@gmail.com>
2021-06-03 11:01:23 -0400

    initial commit

---
+++ src/KAClient.ts
@@ -0,0 +1,76 @@
+class KAClient {
+  KAAS: string;
+  constructor(KAAS: string) {
+    this.KAAS = KAAS;
+  }
+
+  /**
+   * Generates KAAS key from username and password
+   * @return Http status code of login attempt
+   */
+  async fromCredentials(username: string, password: string): Promise<number> {
+    let res = await fetch(
+      "https://www.khanacademy.org/api/internal/graphql/loginWithPasswordMutation",
+      {
+        credentials: "include",
+        headers: {
+          "content-type": "application/json",
+          "x-ka-fkey": "none",
+          cookie: "fkey=none",
+        },
+        body: JSON.stringify({
+          operationName: "loginWithPasswordMutation",
+          query:
+            "mutation loginWithPasswordMutation($identifier: String!, $password: String!) {\n  loginWithPassword(identifier: $identifier, password: $password) {\n    user {\n      id\n      kaid\n      canAccessDistrictsHomepage\n      isTeacher\n      hasUnresolvedInvitations\n      transferAuthToken\n      preferredKaLocale {\n        id\n        kaLocale\n        status\n        __typename\n      }\n      __typename\n    }\n    isFirstLogin\n    error {\n      code\n      __typename\n    }\n    __typename\n  }\n}",
+          variables: {
+            identifier: username,
+            password: password,
+          },
+        }),
+        method: "POST",
+        mode: "cors",
+      }
+    );
+
+    let data = await res.headers.get("set-cookie");
+    this.KAAS = "error";
+    if (data) {
+      let matchKAAS = data.match(/KAAS=([\w-]+)/);
+      if (matchKAAS) {
+        this.KAAS = matchKAAS[1];
+      }
+    }
+
+    return res.status;
+  }
+}
+//So we need a few things...
+//Comment interface, so we can have functions that accept comments to respond/load comments on tthe,
+
+//Ok discovered a few things...
+//After making an initial comment upon a project, the KAencrypted key that is returned can be used to make a comment on that comment.
+
+export interface commentAuthorBase {
+  authorAvatarName: string;
+  authorAvatarSrc: string;
+  authorKaid: string;
+  authorNickname: string;
+}
+
+export interface Comment {
+  answerCount: number;
+  appearsAsDeleted: boolean;
+
+  key: string;
+  badges: any; // Not implimenting badgess until later
+  content: string;
+  date: string;
+  /**
+   * Only true if message is from a gaurdian.
+   */
+  definitelyNotSpam: boolean;
+  /**
+   *
+   */
+  downVoted: boolean;
+}
---
+++ src/renderComment.ts
@@ -0,0 +1,116 @@
+export function renderComment(comment: Comment) {
+  return `
+    <!DOCTYPE html>
+    <html> 
+        <head>
+            <style>
+            ._1732g80e { -webkit-box-align: center !important; -ms-flex-align: center !important; -webkit-box-pack: center !important; -ms-flex-pack: center !important; -webkit-box-direction: normal !important; -webkit-box-orient: horizontal !important; border-width: 0px !important; border-style: solid !important; -moz-box-sizing: border-box !important; box-sizing: border-box !important; display: -webkit-box !important; display: -moz-box !important; display: -ms-flexbox !important; display: -webkit-flex !important; display: flex !important; margin: 0px !important; padding: 0px !important; position: relative !important; z-index: 0 !important; min-height: 0px !important; border-radius: 50% !important; width: 48px !important; min-width: 48px !important; height: 48px !important; border: solid 2px rgba(33,36,44,0.16) !important; background-color: #FFFFFF !important; -webkit-flex-direction: row !important; -ms-flex-direction: row !important; flex-direction: row !important; -webkit-justify-content: center !important; justify-content: center !important; -webkit-align-items: center !important; align-items: center !important; }
+            ._9phhti {
+                width: 32px !important;
+                height: 32px !important;
+            }
+            a:link, a:visited, header a:visited, footer a:visited, .visited-no-recolor a:visited, a.visited-no-recolor:visited {
+                color: #005987;
+            }
+            ._dwmetq {
+                background-color: transparent !important;
+                color: inherit !important;
+                text-decoration: none !important;
+            }
+            ._iat579e {
+                -webkit-box-direction: normal !important;
+                -webkit-box-orient: vertical !important;
+                -webkit-box-align: stretch !important;
+                -ms-flex-align: stretch !important;
+                -webkit-align-items: stretch !important;
+                align-items: stretch !important;
+                border-width: 0px !important;
+                border-style: solid !important;
+                -moz-box-sizing: border-box !important;
+                box-sizing: border-box !important;
+                display: -webkit-box !important;
+                display: -moz-box !important;
+                display: -ms-flexbox !important;
+                display: -webkit-flex !important;
+                display: flex !important;
+                -webkit-flex-direction: column !important;
+                -ms-flex-direction: column !important;
+                flex-direction: column !important;
+                margin: 0px !important;
+                padding: 0px !important;
+                position: relative !important;
+                z-index: 0 !important;
+                min-height: 0px !important;
+                min-width: 0px !important;
+                margin-right: 24px !important;
+            }
+            ._1t544yo9 {
+                -webkit-box-align: start !important;
+                -ms-flex-align: start !important;
+                -webkit-box-direction: normal !important;
+                -webkit-box-orient: horizontal !important;
+                border-width: 0px !important;
+                border-style: solid !important;
+                -moz-box-sizing: border-box !important;
+                box-sizing: border-box !important;
+                display: -webkit-box !important;
+                display: -moz-box !important;
+                display: -ms-flexbox !important;
+                display: -webkit-flex !important;
+                display: flex !important;
+                margin: 0px !important;
+                padding: 0px !important;
+                position: relative !important;
+                z-index: 0 !important;
+                min-height: 0px !important;
+                min-width: 0px !important;
+                -webkit-flex-direction: row !important;
+                -ms-flex-direction: row !important;
+                flex-direction: row !important;
+                -webkit-align-items: flex-start !important;
+                align-items: flex-start !important;
+            }
+            ._xu2jcg {
+                -webkit-box-direction: normal !important;
+                -webkit-box-orient: vertical !important;
+                -webkit-box-align: stretch !important;
+                -ms-flex-align: stretch !important;
+                -webkit-align-items: stretch !important;
+                align-items: stretch !important;
+                border-width: 0px !important;
+                border-style: solid !important;
+                -moz-box-sizing: border-box !important;
+                box-sizing: border-box !important;
+                display: -webkit-box !important;
+                display: -moz-box !important;
+                display: -ms-flexbox !important;
+                display: -webkit-flex !important;
+                display: flex !important;
+                -webkit-flex-direction: column !important;
+                -ms-flex-direction: column !important;
+                flex-direction: column !important;
+                margin: 0px !important;
+                padding: 0px !important;
+                position: relative !important;
+                z-index: 0 !important;
+                min-height: 0px !important;
+                min-width: 0px !important;
+            }
+            </style>
+        </head>
+        <body>
+            <div class="comment-wrapper">
+                <div class="_iat579e">
+                <span class="">
+                    <a aria-label="starky seed style avatar for user DogWhich Go to DogWhich's user profile." href="/profile/kaid_494647823218690861542359/discussion" class="_dwmetq">
+                    <div class="_1732g80e">
+                        <img class="_9phhti" src="file:///Users/beckettpersonal/Downloads/starky-seed.svg" alt="starky seed style avatar for user DogWhich">
+                    </div>
+                    </a> 
+                </span>
+                </div>
+            </div>
+        </body>
+    
+    </html>`;
+}
